a8a65b76eeeb54259fe67dedacbbe475f04e1f65
Dan Lynch <pyramation@gmail.com>
2025-12-24 16:56:52 -0800

    remove extensions

--- extensions/@pgpm/inflection/revert/schemas/inflection/procedures/slugify.sql
+++
@@ -1,8 +0,0 @@
--- Revert schemas/inflection/procedures/slugify from pg
-
-BEGIN;
-
-DROP FUNCTION inflection.slugify(text);
-DROP FUNCTION inflection.slugify(text, boolean);
-
-COMMIT;
--- extensions/@pgpm/inflection/verify/schemas/inflection/procedures/underscore.sql
+++
@@ -1,8 +0,0 @@
--- Verify schemas/inflection/procedures/underscore  on pg
-
-BEGIN;
-
-SELECT verify_function ('inflection.underscore(text)');
-SELECT verify_function ('inflection.underscore(text[])');
-
-ROLLBACK;
