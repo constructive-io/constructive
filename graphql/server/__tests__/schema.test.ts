f492717ef7c14fa6e1d622da687529072003f64f
Zhi Zhen <zetazz@163.com>
2025-12-27 13:47:21 +0800

    fix graphql server schema test

--- graphql/server/__tests__/schema.test.ts
+++ graphql/server/__tests__/schema.test.ts
@@ -2,6 +2,7 @@ process.env.LOG_SCOPE = 'graphile-test';
 
 import { join } from 'path';
 import express from 'express';
+import { buildSchema, GraphQLInputObjectType, GraphQLObjectType } from 'graphql';
 import { postgraphile } from 'postgraphile';
 // @ts-ignore
 import { getGraphileSettings } from 'graphile-settings';
@@ -13,11 +14,39 @@ import { seed, getConnections } from 'graphile-test';
 jest.setTimeout(30000);
 
 const schemas = ['app_public'];
-const sql = (f: string) => join(__dirname, '../../@constructive-io/graphql-test/sql', f);
+const sql = (f: string) => join(__dirname, '../../test/sql', f);
 
 let db: any;
 let teardown: () => Promise<void>;
 let server: import('http').Server | null = null;
+
+const expectSchemaDslBasics = (sdl: string) => {
+  const schema = buildSchema(sdl);
+
+  const queryType = schema.getType('Query') as GraphQLObjectType;
+  expect(queryType).toBeInstanceOf(GraphQLObjectType);
+  const queryFields = queryType.getFields();
+  expect(Object.keys(queryFields)).toEqual(expect.arrayContaining(['users', 'user', 'userByUsername']));
+
+  const userType = schema.getType('User') as GraphQLObjectType;
+  expect(userType).toBeInstanceOf(GraphQLObjectType);
+  const userFields = userType.getFields();
+  expect(userFields.id.type.toString()).toBe('Int!');
+  expect(userFields.username.type.toString()).toBe('String!');
+
+  const mutationType = schema.getType('Mutation') as GraphQLObjectType;
+  expect(mutationType).toBeInstanceOf(GraphQLObjectType);
+  const mutationFields = mutationType.getFields();
+  expect(Object.keys(mutationFields)).toEqual(expect.arrayContaining(['createUser', 'updateUser', 'deleteUser']));
+
+  const createUserInput = schema.getType('CreateUserInput') as GraphQLInputObjectType;
+  expect(createUserInput).toBeInstanceOf(GraphQLInputObjectType);
+  expect(createUserInput.getFields().user.type.toString()).toBe('UserInput!');
+
+  const userInput = schema.getType('UserInput') as GraphQLInputObjectType;
+  expect(userInput).toBeInstanceOf(GraphQLInputObjectType);
+  expect(userInput.getFields().username.type.toString()).toBe('String!');
+};
 
 beforeAll(async () => {
   const connections = await getConnections(
@@ -87,6 +116,7 @@ it('fetchEndpointSchemaSDL returns the same SDL as buildSchemaSDL for a running 
 
   expect(typeof sdlViaHTTP).toBe('string');
   expect(sdlViaHTTP).toContain('type Query');
+  expectSchemaDslBasics(sdlDirect);
 
   // The SDL generated directly from the database should match the one from HTTP introspection
   expect(sdlViaHTTP).toBe(sdlDirect);
