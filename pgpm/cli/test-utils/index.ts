fed382587f33e1346a71514a8b8a593aaa2423e5
Dan Lynch <pyramation@gmail.com>
2025-12-27 05:00:20 +0000

    refactor: migrate test utilities to @inquirerer/test
    - Replace local test utilities in packages/cli/test-utils/cli.ts with re-exports from @inquirerer/test
    - Replace local test utilities in pgpm/cli/test-utils/cli.ts with re-exports from @inquirerer/test
    - Remove redundant normalizePackageJsonForSnapshot export from pgpm/cli/test-utils/index.ts
    - Add @inquirerer/test@^1.1.2 as devDependency to both CLI packages
    - Remove clean-ansi from devDependencies (now provided by @inquirerer/test)
    - Update snapshot to show correct <DOWN_ARROW> humanization
    
    This removes ~270 lines of duplicated test utility code.

--- pgpm/cli/test-utils/index.ts
+++ pgpm/cli/test-utils/index.ts
@@ -3,4 +3,3 @@ export * from './CLIDeployTestFixture';
 export * from './fixtures';
 export * from './init-argv';
 export * from './TestDatabase';
-export { normalizePackageJsonForSnapshot } from './cli';
